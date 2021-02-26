61a95461362a7bfcacba7882b20b9ca5dcb0fb27
Liudmila Budanitskaya <liudmila28march1991@gmail.com>
2021-02-26 16:31:47 +0300

    initial commit

---
+++ homework1/src/main/java/site/budanitskaya/homework1/MainActivity.kt
@@ -0,0 +1,72 @@
+package site.budanitskaya.homework1
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    var button12: AppCompatButton? = null
+    var button13: AppCompatButton? = null
+    var clickListener: View.OnClickListener? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button12 = findViewById(R.id.button12)
+        button13 = findViewById(R.id.button13)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        clickListener = this
+        button12?.setOnClickListener(clickListener)
+        button13?.setOnClickListener(clickListener)
+    }
+
+    override fun onClick(p0: View?) {
+
+        when (p0?.id) {
+            R.id.button12 -> startActivity(ActivityTwo.createIntent())
+            R.id.button13 -> startActivity(ActivityThree.createIntent())
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (clickListener != null) clickListener = null
+
+    }
+
+    companion object {
+        fun createIntent(): Intent = Intent("activity1")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_items, menu)
+        return true
+    }
+
+
+    override fun onPrepareOptionsMenu(menu: Menu): Boolean {
+
+
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.share -> startActivity(Intent.createChooser(Intent(Intent.ACTION_SEND), null))
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
