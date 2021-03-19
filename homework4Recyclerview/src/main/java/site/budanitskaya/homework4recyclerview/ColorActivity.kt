e29e6a978cd4d29ba4e75ff9154b51059392f4ba
Liudmila Budanitskaya <liudmila28march1991@gmail.com>
2021-03-19 20:38:50 +0300

    copy to clipboard feature added

--- homework4Recyclerview/src/main/java/site/budanitskaya/homework4recyclerview/ColorActivity.kt
+++ homework4Recyclerview/src/main/java/site/budanitskaya/homework4recyclerview/ColorActivity.kt
@@ -3,47 +3,54 @@ package site.budanitskaya.homework4recyclerview
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
-import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import site.budanitskaya.homework4recyclerview.DataUtils.Companion.EXTRA_MESSAGE
 import site.budanitskaya.homework4recyclerview.databinding.ActivityColorBinding
-import site.budanitskaya.homework4recyclerview.databinding.ActivityMainBinding
+
 
 class ColorActivity : AppCompatActivity() {
 
     var pasteData: String = ""
+    lateinit var clipboard: ClipboardManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         val binding =
             DataBindingUtil.setContentView<ActivityColorBinding>(this, R.layout.activity_color)
-        val message = intent.getStringExtra(EXTRA_MESSAGE)
-        val colorView = binding.colorView
-        colorView.setBackgroundColor(Color.parseColor(message))
 
-        binding.copyColor.setOnClickListener {
-            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-            val clip: ClipData = ClipData.newPlainText("simple text", message)
+        val stringForColorFromRecyclerView = intent.getStringExtra(EXTRA_MESSAGE)
+        val colorBackground = binding.colorBackground
+
+        colorBackground.setBackgroundColor(Color.parseColor(stringForColorFromRecyclerView))
+
+        // button to copy color data to clipboard
+
+        binding.copyColorButton.setOnClickListener {
+
+            clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip: ClipData =
+                ClipData.newPlainText("Color Code", stringForColorFromRecyclerView)
             clipboard.setPrimaryClip(clip)
-            Toast.makeText(this, "Copied to clipboard!", Toast.LENGTH_LONG).show()
+
+            Toast.makeText(this, "Color code copied to clipboard!", Toast.LENGTH_LONG).show()
         }
 
-        binding.pasteColor.setOnClickListener {
-            var clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        // button to paste color data from clipboard
 
-            val item = clipboard.primaryClip?.getItemAt(0)
+        binding.pasteColorButton.setOnClickListener {
 
-            // Gets the clipboard as text.
-            pasteData = item?.text as String
+            clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clipBoardItem = clipboard.primaryClip?.getItemAt(0)
+            pasteData = clipBoardItem?.text as String
 
             binding.textField.setText(pasteData)
 
-            Toast.makeText(this, "Fetched from clipboard!", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Color code fetched from clipboard!", Toast.LENGTH_LONG).show()
         }
-
     }
 }
