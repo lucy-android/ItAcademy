2a1348e5a057e2a987d7b20cb82c24c831b8c046
Liudmila Budanitskaya <liudmila28march1991@gmail.com>
2021-03-27 08:13:45 +0300

    database insert working

--- todolist/src/main/java/site/budanitskaya/todolist/MainActivity.kt
+++ todolist/src/main/java/site/budanitskaya/todolist/MainActivity.kt
@@ -2,10 +2,26 @@ package site.budanitskaya.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import site.budanitskaya.todolist.database.Task
+import site.budanitskaya.todolist.database.TaskDatabase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val taskDataBase = TaskDatabase.getInstance(applicationContext)
+
+        val taskDatabaseDao = taskDataBase.taskDao()
+
+        val task = Task()
+        task.taskTitle = "Get up!"
+        task.taskDescription = "Wake up early in the morning and get up from bed"
+
+        if(taskDatabaseDao != null) {
+            taskDatabaseDao.insert(task)
+            Log.d("3141592", "onCreate: ${task.taskTitle}, ${task.taskDescription}")
+        }
     }
 }
--- todolist/src/main/java/site/budanitskaya/todolist/database/TaskDatabase.kt
+++ todolist/src/main/java/site/budanitskaya/todolist/database/TaskDatabase.kt
@@ -1,10 +1,29 @@
 package site.budanitskaya.todolist.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 
 @Database(entities = [Task::class], version = 1)
 abstract class TaskDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDatabaseDao?
+
+    companion object {
+
+        private var INSTANCE: TaskDatabase? = null
+        fun getInstance(context: Context): TaskDatabase {
+
+            var instance = INSTANCE
+            if (instance == null) {
+                instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TaskDatabase::class.java, "todo_list_table"
+                ).allowMainThreadQueries().build()
+                INSTANCE = instance
+            }
+            return instance
+        }
+    }
 }
