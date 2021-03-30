5989b0cd6d2e206c4113b3123d2c0932143b2dd9
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2021-03-30 23:11:01 +0300

    item removed from recyclerview

--- todolist/src/main/java/site/budanitskaya/todolist/firstscreen/FirstFragment.kt
+++ todolist/src/main/java/site/budanitskaya/todolist/firstscreen/FirstFragment.kt
@@ -21,13 +21,19 @@ class FirstFragment : Fragment() {
 
     var it: Int? = null
 
+    private lateinit var adapter: ToDoListAdapter
+
+    private lateinit var recyclerView: RecyclerView
+
+    private lateinit var tasks: MutableList<Task>
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_first, container, false)
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
 
         val taskDataBase = TaskDatabase.getInstance(requireContext())
 
@@ -35,9 +41,9 @@ class FirstFragment : Fragment() {
 
         Log.d("", "onCreateView: $taskDatabaseDao")
 
-        val taskList = taskDatabaseDao.getTaskList()
-        val adapter = ToDoListAdapter(
-            taskList
+        tasks = taskDatabaseDao.getTaskList().toMutableList()
+        adapter = ToDoListAdapter(
+                tasks
         ) {
             this.it = it
 
@@ -49,7 +55,10 @@ class FirstFragment : Fragment() {
                     actionModeCallback.actionMode = activity?.startActionMode(actionModeCallback)
                     view.isSelected = true
                 }
+
             }
+
+
 
             return@ToDoListAdapter true
 
@@ -60,7 +69,6 @@ class FirstFragment : Fragment() {
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = adapter
 
-        recyclerView.post { adapter.notifyDataSetChanged() }
 
         val fab = view.findViewById<FloatingActionButton>(R.id.fab)
 
@@ -76,47 +84,45 @@ class FirstFragment : Fragment() {
         inflater.inflate(R.menu.context, menu)
         super.onCreateOptionsMenu(menu, inflater)
     }
-}
 
-class ActionModeCallBackImpl(val context: Context, val position: Int) : ActionMode.Callback {
+    inner class ActionModeCallBackImpl(val context: Context, val position: Int) : ActionMode.Callback {
 
-    var actionMode: ActionMode? = null
+        var actionMode: ActionMode? = null
 
-    override fun onCreateActionMode(p0: ActionMode?, p1: Menu?): Boolean {
-        // Inflate a menu resource providing context menu items
-        val inflater: MenuInflater = p0!!.menuInflater
-        inflater.inflate(R.menu.context, p1)
-        return true
-    }
-
-    override fun onPrepareActionMode(mode: ActionMode, menu: Menu): Boolean {
-        return false // Return false if nothing is done
-    }
-
-    // Called when the user selects a contextual menu item
-    override fun onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean {
-
-        when (item.itemId) {
-            R.id.delete -> {
-
-                val taskDataBase = TaskDatabase.getInstance(context)
-
-                val taskDatabaseDao = taskDataBase.taskDao()!!
-
-                val task: Task = taskDatabaseDao.getTaskList()[position]
-
-                taskDatabaseDao.delete(task)
-
-            }
-            // как здесь получить элемент, который был нажат?
+        override fun onCreateActionMode(p0: ActionMode?, p1: Menu?): Boolean {
+            // Inflate a menu resource providing context menu items
+            val inflater: MenuInflater = p0!!.menuInflater
+            inflater.inflate(R.menu.context, p1)
+            return true
         }
-        return true
-    }
 
-    // Called when the user exits the action mode
-    override fun onDestroyActionMode(mode: ActionMode) {
-        actionMode = null
+        override fun onPrepareActionMode(mode: ActionMode, menu: Menu): Boolean {
+            return false // Return false if nothing is done
+        }
+
+        // Called when the user selects a contextual menu item
+        override fun onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean {
+
+            when (item.itemId) {
+                R.id.delete -> {
+
+                    val task: Task = tasks[position]
+                    val taskDataBase = TaskDatabase.getInstance(requireContext())
+
+                    val taskDatabaseDao = taskDataBase.taskDao()!!
+                    taskDatabaseDao.delete(task)
+                    tasks.remove(task)
+                    recyclerView.removeViewAt(it!!)
+                    adapter.notifyItemRemoved(it!!)
+                    adapter.notifyItemRangeChanged(it!!, tasks.size)
+                }
+            }
+            return true
+        }
+
+        // Called when the user exits the action mode
+        override fun onDestroyActionMode(mode: ActionMode) {
+            actionMode = null
+        }
     }
 }
-
-
